e3b72147a67cad606138cacf237d3643c8152996
Phillip Francis Chapita <philoc95@live.com>
2024-08-01 22:27:01 +0800

    Added the speed insights component to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import './App.css'
-import Header from './components/nav/header'
-import Hero from './components/hero'
-import Projects from './components/projects'
-import Contact from './components/contact'
-import Footer from './components/nav/footer'
-import About from './components/about'
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import "./App.css";
+import Header from "./components/nav/header";
+import Hero from "./components/hero";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
+import Footer from "./components/nav/footer";
+import About from "./components/about";
 
 function App() {
-
   return (
     <>
-    <Header />
-    <Hero />  
-    <About />
-    <Projects />
-    <Contact />
-    <Footer />
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+      <SpeedInsights />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
