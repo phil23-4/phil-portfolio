d03b2b5ae47464126a83b26882d4aef18b8dc764
Phillip Francis Chapita <philoc95@live.com>
2024-08-09 01:15:27 +0800

    fixed download links

--- src/components/nav/header.jsx
+++ src/components/nav/header.jsx
@@ -164,7 +164,8 @@ export default function Header() {
           </div>
           <a
             className="inline-flex justify-center rounded-lg text-sm font-semibold py-2.5 px-4 bg-slate-900 text-white hover:bg-slate-700  ml-8"
-            href="/all-access"
+            href="/resume.pdf"
+            target="_blank"
           >
             <span>
               Download CV <span aria-hidden="true">→</span>
