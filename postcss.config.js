c717ab2e11ca129dd8db5c6a4cd4885e95243518
Phillip Francis Chapita <philoc95@live.com>
2024-08-16 19:18:27 +0800

    removed unused packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-import process from "process";
-
 export default {
   plugins: {
-    "tailwindcss/nesting": {},
-    "tailwindcss/postcss7-compat": {},
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 };
